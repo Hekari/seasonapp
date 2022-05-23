d621ed923880443804783753282ac5b29d039467
Hekari <sszpytma6@gmail.com>
2022-05-23 19:45:02 +0200

    GeolocationApp Commit

---
+++ src/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Spinner(props) {
+  return (
+    <div className="ui active inverted dimmer">
+      <div className="ui text loader">{props.message}</div>
+    </div>
+  );
+}
+Spinner.defaultProps = {
+  message: "Loading...",
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./style.scss";
+import { createRoot } from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay.js";
+import Spinner from "./Spinner.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.querySelector("#root");
+const root = createRoot(container);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+  componentDidUpdate() {
+    console.log(`component has just updated itself!`);
+  }
+  renderContent() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
+    } else if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    } else {
+      return <Spinner message="Awaiting your location request" />;
+    }
+  }
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+root.render(<App></App>);
